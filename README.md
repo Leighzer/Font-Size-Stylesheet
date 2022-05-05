df9e7ca6e5dc650dfee28359e8e74fe172a4113e
Leighzer <bwc_nothgiel@live.com>
2022-05-05 01:16:24 -0400

    init commit;

---
+++ README.md
@@ -0,0 +1,18 @@
+# Font-Size-Stylesheet
+Bootstrap 5 compatible stylesheet that gives the ability to responsively resize text as screen size changes.
+
+# Example
+The following markup:
+
+<h1 class="fs-10px fs-sm-20px fs-md-30px fs-lg-40px fs-xl-50px fs-xxl-60px">
+
+Will have:
+10px font size for extra small screens
+
+20px font size on small screens
+
+30px font size on medium screens... etc.
+
+As the screen is resized - you can responsively control the font size of text.
+
+(Yes this could probably be simplified and perhaps better integrated with bootstrap if this was implemented in Sass)
---
+++ fontsizegen/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace fontsizegen
+{
+    // simple throw away program to generate font-size.css stylesheet
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int maxPx = 256;
+            Dictionary<string, int> breakpoints = new Dictionary<string, int>
+            {
+                //{ "", 0 },
+                { "sm", 576 },
+                { "md", 768 },
+                { "lg", 992 },
+                { "xl", 1200 },
+                { "xxl", 1400 },
+            };
+
+            // this block covers xs styles since no media querys are required
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= maxPx; i++)
+            {
+                int minWidth = i;
+                sb.AppendLine($".fs-{minWidth}px\n{{\n\tfont-size: {minWidth}px;\n}}\n");
+            }
+
+            foreach (var kv in breakpoints)
+            {
+                int minWidth = kv.Value;
+                sb.AppendLine($"@media (min-width: {minWidth}px)\n{{\t");
+                for (int i = 1; i <= maxPx; i++)
+                {   
+                    sb.AppendLine($"\t.fs-{kv.Key}-{i}px\n\t{{\n\t\tfont-size: {i}px;\n\t}}\n");
+                }
+                sb.AppendLine($"}}\n");
+            }
+            File.WriteAllText("../../../../font-size.css", sb.ToString());
+        }
+    }
+}
